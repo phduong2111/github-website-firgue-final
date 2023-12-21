3cf31392004d2b6328fd680bbf5226e9b2234179
phduong2111 <phduong2111@gmail.com>
2023-12-21 18:57:29 +0700

    Initial commit

---
+++ luanvan_PRE-FINAL/mvc/views/client/inc/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer-content">
+      <p>Design & Develop by ĐỒ ÁN PRO TEAM</p>
+      <p>Phạm Hoàng Dương-21/11/2002</p>
+      <p>Mai Ngọc Đức-09/08/2002</p>
+      <p>Nguyễn Trọng Hiếu-07/02/2002</p>
+    </div>
+    <ul class="socials">
+      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+      <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+    </ul>
+  </footer>
